b33e754437a446e06615adec734a67906b82c46b
ShuaiHua Du <shuaihua.du@outlook.com>
2024-01-18 13:40:51 +0800

    Initial commit.

---
+++ quickstarts/KernelMemoryExamples/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using KernelMemoryExamples;
+global using KernelMemoryExamples.Reliability;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Logging;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
---
+++ quickstarts/KernelMemoryExamples/RepoUtils/ConfigurationException.cs
@@ -0,0 +1,16 @@
+﻿namespace KernelMemoryExamples.RepoUtils;
+
+public class ConfigurationException : Exception
+{
+    public ConfigurationException()
+    {
+    }
+
+    public ConfigurationException(string message) : base(message)
+    {
+    }
+
+    public ConfigurationException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
